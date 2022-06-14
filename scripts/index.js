d58bd4c5eee7fae175e93de3c81b14febb74ca50
Maxim Orlov <Max40287@yandex.ru>
2022-06-15 00:36:40 +0300

    карточки при загрузке

--- scripts/index.js
+++ scripts/index.js
@@ -35,3 +35,43 @@ form.addEventListener('submit', function formSubmitHandler(evt) {
     document.querySelector('.profile__subname').innerHTML = subnameInput.value;
     popupProfile.classList.remove('popup_opened');
 });
+
+/* Шесть карточек «из коробки» */
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+const cardsList = document.querySelector('.cards__list');
+const cardTemplate = document.querySelector('#card-template').content;
+
+initialCards.forEach(function (element) {
+    const cardElement = cardTemplate.cloneNode(true);
+
+    cardElement.querySelector('.card__title').textContent = element.name;
+    cardElement.querySelector('.card__image').src = element.link;
+
+    cardsList.append(cardElement);
+});
